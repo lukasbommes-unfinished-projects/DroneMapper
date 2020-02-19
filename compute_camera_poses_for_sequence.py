3906b33750832fc6d525ac30c6b10a92991cd6ba
LukasBommes <mail@lukasbommes.de>
2020-02-19 12:00:32 +0800

    reference to last keyframe insteadf of previous frame

--- compute_camera_poses_for_sequence.py
+++ compute_camera_poses_for_sequence.py
@@ -5,6 +5,7 @@ from ssc import ssc
 
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from pytransform3d.rotations import *
 
 # camera parameters
 w = 1920
@@ -25,15 +26,35 @@ new_camera_matrix = camera_matrix
 #new_camera_matrix, _ = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coeffs, (w, h), alpha=0)
 mapx, mapy = cv2.initUndistortRectifyMap(camera_matrix, dist_coeffs, None, new_camera_matrix, (w, h), cv2.CV_32FC1)
 
-cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("frame", 1600, 900)
-
 orb = cv2.ORB_create()
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 fast = cv2.FastFeatureDetector_create(threshold=12)
 num_ret_points = 3000
 tolerance = 0.1
 num_matches = 50
+
+
+def extract_kp_des(current_frame, fast, orb, draw=True):
+    kp = fast.detect(current_frame, None)
+    kp = sorted(kp, key = lambda x:x.response, reverse=True)
+    kp = ssc(kp, num_ret_points, tolerance, current_frame.shape[1], current_frame.shape[0])
+    kp, des = orb.compute(current_frame, kp)
+    if draw:
+        current_frame = cv2.drawKeypoints(current_frame, kp, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    return kp, des, current_frame
+
+
+def match(last_keyframe, current_frame, bf, last_des, des, last_kp, kp, draw=True):
+    matches = bf.match(last_des, des)
+    matches = sorted(matches, key = lambda x:x.distance)
+    print("Found {} matches of current frame with last frame".format(len(matches)))
+    # get matched keypoints
+    last_pts = np.array([last_kp[m.queryIdx].pt for m in matches[:num_matches]]).reshape(1, -1, 2)
+    current_pts = np.array([kp[m.trainIdx].pt for m in matches[:num_matches]]).reshape(1, -1, 2)
+    if draw:
+        match_frame = cv2.drawMatches(last_keyframe, last_kp, current_frame, kp, matches[:num_matches], None, matchColor=(0, 0, 0), flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    return matches, last_pts, current_pts, match_frame
+
 
 cv2.namedWindow("last_frame", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("last_frame", 1600, 900)
@@ -42,12 +63,13 @@ cv2.resizeWindow("current_frame", 1600, 900)
 cv2.namedWindow("match_frame", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("match_frame", 1600, 900)
 
+step_wise = True
 
 # 3D plot
 # fig = plt.figure()
 # ax = fig.add_subplot(111, projection='3d')
-# #ax.scatter(src_pts.reshape(-1, 2)[:, 0], src_pts.reshape(-1, 2)[:, 1], np.zeros(src_pts.shape[1]))
-# #ax.scatter(dst_pts.reshape(-1, 2)[:, 0], dst_pts.reshape(-1, 2)[:, 1], np.zeros(dst_pts.shape[1]))
+# #ax.scatter(last_pts.reshape(-1, 2)[:, 0], last_pts.reshape(-1, 2)[:, 1], np.zeros(last_pts.shape[1]))
+# #ax.scatter(current_pts.reshape(-1, 2)[:, 0], current_pts.reshape(-1, 2)[:, 1], np.zeros(current_pts.shape[1]))
 # ax.scatter(triangulatedPoints[:, 0], triangulatedPoints[:, 1], triangulatedPoints[:, 2])
 # ax.set_xlabel("x")
 # ax.set_ylabel("y")
@@ -58,18 +80,30 @@ cv2.resizeWindow("match_frame", 1600, 900)
 # discard intial part of video
 [cap.read() for i in range(1000)]
 
-last = {
+last_kf = {
     "frame": None,
-    "kp": None,
-    "des": None
+    "key_points": None,
+    "descriptors": None,
 }
-
-loaded_intial_frame = False
+match_frame = None
 
 Rs = []
 ts = []
+map_points = []
 
 frame_idx = 0
+
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111, projection='3d')
+ax1.set_xlabel("x")
+ax1.set_ylabel("y")
+ax1.set_zlabel("z")
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+ax2.set_xlabel("x")
+ax2.set_ylabel("y")
+
 
 while(True):
 
@@ -78,53 +112,77 @@ while(True):
     retc, current_frame = cap.read()
     current_frame = cv2.remap(current_frame, mapx, mapy, cv2.INTER_CUBIC)
 
-    kp = fast.detect(current_frame, None)
-    kp = sorted(kp, key = lambda x:x.response, reverse=True)
+    kp, des, current_frame = extract_kp_des(current_frame, fast, orb, draw=True)
     print("Found {} keypoint in current frame".format(len(kp)))
 
-    kp = ssc(kp, num_ret_points, tolerance, current_frame.shape[1], current_frame.shape[0])
-    kp, des = orb.compute(current_frame, kp)
+    if frame_idx == 0:
+        last_kf["frame"] = current_frame
+        last_kf["key_points"] = kp
+        last_kf["descriptors"] = des
+        frame_idx += 1
+        continue
 
-    if loaded_intial_frame:
+    matches, last_pts, current_pts, match_frame = match(last_kf["frame"], current_frame, bf, last_kf["descriptors"], des, last_kf["key_points"], kp, draw=True)
 
-        last["frame"] = cv2.drawKeypoints(last["frame"], last["kp"], None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        current_frame = cv2.drawKeypoints(current_frame, kp, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # recover camera pose from point correspondences
+    essential_mat, _ = cv2.findEssentialMat(last_pts, current_pts, camera_matrix, method=cv2.RANSAC)
+    retval, R, t, mask = cv2.recoverPose(essential_mat, last_pts, current_pts, camera_matrix)
+    Rs.append(R)
+    ts.append(t)
+    map_points.append(current_pts)
 
-        matches = bf.match(last["des"], des)
-        matches = sorted(matches, key = lambda x:x.distance)
-        print("Found {} matches of current frame with last frame".format(len(matches)))
+    print("R", R)
+    print("t", t)
+    print(retval)
 
-        match_frame = cv2.drawMatches(last["frame"], last["kp"], current_frame, kp, matches[:num_matches], None, matchColor=(0, 0, 0), flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    plot_basis(ax1, R, t.reshape(3,))
 
-        # get matched keypoints
-        src_pts = np.array([last["kp"][m.queryIdx].pt for m in matches[:num_matches]]).reshape(1, -1, 2)
-        dst_pts = np.array([kp[m.trainIdx].pt for m in matches[:num_matches]]).reshape(1, -1, 2)
+    ax2.scatter(last_pts.reshape(-1, 2)[:, 0], last_pts.reshape(-1, 2)[:, 1])
+    ax2.scatter(current_pts.reshape(-1, 2)[:, 0], current_pts.reshape(-1, 2)[:, 1])
 
-        # recover camera pose from point correspondences
-        essential_mat, _ = cv2.findEssentialMat(src_pts, dst_pts, camera_matrix, method=cv2.RANSAC)
-        retval, R, t, mask = cv2.recoverPose(essential_mat, src_pts, dst_pts, camera_matrix)
-        Rs.append(R)
-        ts.append(t)
-
-        cv2.imshow("last_frame", last["frame"])
-        cv2.imshow("current_frame", current_frame)
+    cv2.imshow("current_frame", current_frame)
+    if last_kf["frame"] is not None:
+        cv2.imshow("last_frame", last_kf["frame"])
+    if match_frame is not None:
         cv2.imshow("match_frame", match_frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
 
-    # update last infos for next iteration
-    last["frame"] = current_frame
-    last["kp"] = kp
-    last["des"] = des
+    # handle key presses
+    # 'q' - Quit the running program
+    # 's' - enter stepwise mode
+    # 'a' - exit stepwise mode
+    key = cv2.waitKey(1)
+    if not step_wise and key == ord('s'):
+        step_wise = True
+    if key == ord('q'):
+        break
+    if step_wise:
+        while True:
+            key = cv2.waitKey(1)
+            if key == ord('s'):
+                break
+            elif key == ord('a'):
+                step_wise = False
+                break
 
-    loaded_intial_frame = True
+    # TODO: define a robust thresholding mechanism to decide when to insert a new keyframe
+    # first compute threshold
+
+    # update last infos for next iteration (ONLY DO THIS WHEN THRESHOLD IS EXCEEDED)
+    #last_kf["frame"] = current_frame
+    #last_kf["key_points"] = kp
+    #last_kf["descriptors"] = des
+
     frame_idx += 1
 
-    if frame_idx == 10:
+    if frame_idx == 100:
         break
 
 cap.release()
-#cv2.destroyAllWindows()
+cv2.destroyAllWindows()
 
-print(ts)
-print(Rs)
+plt.show()
+
+import pickle
+pickle.dump(Rs, open("Rs.pkl", "wb"))
+pickle.dump(ts, open("ts.pkl", "wb"))
+pickle.dump(map_points, open("map_points.pkl", "wb"))
